e3a299420e81b5bf2d6ab401d63665c3bb5aae4c
liatmoss <liatmoss@hotmail.com>
2019-10-03 15:08:26 +0100

    Completed exercise 7

--- directory1.rb
+++ directory1.rb
@@ -7,37 +7,41 @@ def input_students
   name = gets.chomp
   puts "Students favourite hobby"
   hobby = gets.chomp
+  puts "Student cohort"
+  cohort = gets.chomp.to_sym
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november, hobby: hobby}
+    students << {name: name, cohort: cohort, hobby: hobby}
     puts "Now we have #{students.count} students"
     # get another name and hobby from the user
     puts "Please enter the names of the students"
     name = gets.chomp
     puts "Students favourite hobby"
     hobby = gets.chomp
+    puts "Student cohort"
+    cohort = gets.chomp.to_sym
   end
   # return the array of students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(50)
-  puts "-------------".center(50)
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 def print(students)
   i = 0
   while i < students.length
-   puts "#{[i + 1]}#{students[i][:name]} (#{students[i][:cohort]} cohort )#{students[i][:hobby]}".center(50)
+   puts "#{[i + 1]}#{students[i][:name]} (#{students[i][:cohort]} cohort) #{students[i][:hobby]}"
    i +=1
   end
 end
 
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(50)
+  puts "Overall, we have #{names.count} great students"
 end
 
 students = input_students
