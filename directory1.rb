e304035d3b7f620c299014e36f3d537672194456
liatmoss <liatmoss@hotmail.com>
2019-10-03 14:44:10 +0100

    completed exercise 5

--- directory1.rb
+++ directory1.rb
@@ -5,13 +5,18 @@ def input_students
   students = []
   # get the first name
   name = gets.chomp
+  puts "Students favourite hobby"
+  hobby = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobby: hobby}
     puts "Now we have #{students.count} students"
-    # get another name from the user
+    # get another name and hobby from the user
+    puts "Please enter the names of the students"
     name = gets.chomp
+    puts "Students favourite hobby"
+    hobby = gets.chomp
   end
   # return the array of students
   students
@@ -25,10 +30,11 @@ end
 def print(students)
   i = 0
   while i < students.length
-   puts "#{[i + 1]}#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+   puts "#{[i + 1]}#{students[i][:name]} (#{students[i][:cohort]} cohort )#{students[i][:hobby]}"
    i +=1
   end
 end
+
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
